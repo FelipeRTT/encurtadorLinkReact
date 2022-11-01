ca06d57649993a170d1952572de4d519a74d325e
Felipe Ranghetti <feliperanghetti4@gmail.com>
2022-11-01 15:58:52 -0300

    1

--- encurtaLink/src/App.jsx
+++ encurtaLink/src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./index.css";
+import teste from "./assets/teste.svg";
 
 const App = () => {
   return (
-    <div>
-      <div className="teste">Olla mundo</div>
+    <div className="flex justify-between ">
+      <div className="flex flex-col items-center justify-center h-screen w-full">
+        <h1 className="mb-10 text-5xl">Encurte seu link!</h1>
+        <input
+          className="bg-black text-white w-4/6 px-8 py-4 rounded-xl border  "
+          type="text"
+          placeholder="Insira o link que você quer encurtar!"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center h-screen w-full">
+        <img src={teste} alt="" className="w-80" />
+      </div>
     </div>
   );
 };
