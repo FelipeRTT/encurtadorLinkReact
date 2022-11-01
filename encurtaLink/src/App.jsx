1c9efdfea4a1593c8703ed46dca9582623646d3d
Felipe Ranghetti <feliperanghetti4@gmail.com>
2022-11-01 15:32:28 -0300

    a

---
+++ encurtaLink/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <div className="teste">Olla mundo</div>
+    </div>
+  );
+};
+
+export default App;
